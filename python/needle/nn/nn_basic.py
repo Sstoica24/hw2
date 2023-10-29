42895ce30b09dbcb35317dce2dc5ad2d4c11c810
Sstoica24 <sofiastoica@proofschool.org>
2023-10-29 00:10:52 -0700

    Done up to Q3 but no logSumExp

--- python/needle/nn/nn_basic.py
+++ python/needle/nn/nn_basic.py
@@ -113,7 +113,11 @@ class Linear(Module):
 class Flatten(Module):
     def forward(self, X):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        shape_list = list(X.shape)
+        prod = 1
+        for i in range(1, len(shape_list)):
+            prod *= shape_list[i]
+        return X.reshape((X.shape[0], prod))
         ### END YOUR SOLUTION
 
 
@@ -140,7 +144,20 @@ class Sequential(Module):
 class SoftmaxLoss(Module):
     def forward(self, logits: Tensor, y: Tensor):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        Z = logits
+        y_one_hot = init.one_hot(Z.shape[1], y, device=None, dtype="float32", requires_grad=False)
+        m = Z.shape[0]
+        exp_Z = ops.exp(Z)
+        #exp_Z is 2 dim, but summation would return 1D, so we need to make summation 2D
+        # based on how the code is written in the backward pass if you don't explicitley call broadcast
+        # then it doesn't know that broadcast was used. 
+        logSumExp = ops.logsumexp(Z)
+        # softmax_probs = exp_Z / ops.summation(exp_Z, axes=(1,)).reshape((exp_Z.shape[0], 1)).broadcast_to((exp_Z.shape) )
+        # Compute the loss for each sample in the batch
+        log_t = ops.log(logSumExp) * y_one_hot
+        loss = -ops.summation(log_t) / m
+        # Average the loss over all samples
+        return loss
         ### END YOUR SOLUTION
 
 
@@ -151,12 +168,37 @@ class BatchNorm1d(Module):
         self.eps = eps
         self.momentum = momentum
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        self.weight = Parameter(init.ones(dim, requires_grad=True))
+        self.bias = Parameter(init.zeros(dim, requires_grad=True))
+        # the following two are not paramters and, of course, do not need grads
+        # are they are not learnable. 
+        self.running_mean = init.zeros(dim)
+        self.running_var = init.ones(dim)
         ### END YOUR SOLUTION
 
     def forward(self, x: Tensor) -> Tensor:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        # module has self.training
+        if self.training:
+            # summing over batch_size ==> summing over axis = 0
+            # note, we do not need to reshape to 2D (x.shape[0], 1) because we are working
+            # with the batch size. 
+            # you can't set batch_mean = (x.sum((0,)) / x.shape[0]).broadcast_to(x.shape)
+            # because the tester expects batch_mean TO NOT BE SIZE BE SIZE OF x.shape (
+            # saw this while I was debugging). Same story for batch_var
+            batch_mean = x.sum((0,)) / x.shape[0]
+            batch_var = ((x - batch_mean.broadcast_to(x.shape))**2).sum((0,)) / x.shape[0]
+            norm = (x - batch_mean.broadcast_to(x.shape)) / (batch_var.broadcast_to(x.shape) + self.eps)**0.5
+            y = self.weight.broadcast_to(x.shape) * norm + self.bias.broadcast_to(x.shape)
+            # what is meant by the equation given is that x_old = old self.running_mean and 
+            # self.running_var and x_observed = the current batch_mean.data and batch_var.data.
+            # .data since batch_mean and batch_var are both tensors 
+            # no need to worry about broadcasting since these have no derivatives!
+            self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * batch_mean.data
+            self.running_var = (1 - self.momentum) * self.running_var + self.momentum * batch_var.data
+        else:
+            y = (x - self.running_mean.broadcast_to(x.shape)) / (self.running_var.broadcast_to(x.shape) + self.eps)**0.5
+        return y
         ### END YOUR SOLUTION
 
 
@@ -166,12 +208,16 @@ class LayerNorm1d(Module):
         self.dim = dim
         self.eps = eps
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
-        ### END YOUR SOLUTION
+        self.weight = Parameter(init.ones(dim, requires_grad=True))
+        self.bias = Parameter(init.zeros(dim, requires_grad=True))
+        ### END YOUR SOLUTION 
 
     def forward(self, x: Tensor) -> Tensor:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        mean = (x.sum((1,))/x.shape[1]).reshape((x.shape[0], 1)).broadcast_to(x.shape)
+        var = (((x - mean)**2).sum((1,))/x.shape[1]).reshape((x.shape[0], 1)).broadcast_to(x.shape)
+        deno = (var + self.eps)**0.5
+        return self.weight.broadcast_to(x.shape) * (x - mean)/deno + self.bias.broadcast_to(x.shape)
         ### END YOUR SOLUTION
 
 
@@ -182,7 +228,13 @@ class Dropout(Module):
 
     def forward(self, x: Tensor) -> Tensor:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        if self.training:
+            # we want z_(x+1)/(1-p) to occur 1-p times ==> prob in ranb must be 1-p
+            bernoulli_mask = init.randb(*x.shape, p=1-self.p, requires_grad=True)
+            return x * bernoulli_mask / (1-self.p)
+        else:
+            # no dropout during testing
+            return x
         ### END YOUR SOLUTION
 
 
@@ -193,5 +245,5 @@ class Residual(Module):
 
     def forward(self, x: Tensor) -> Tensor:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+       return self.fn(x) + x
         ### END YOUR SOLUTION
